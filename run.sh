0713b5a576cab8cd2a69fd604cef26a5563087e5
cheerss <zjdxwwx@163.com>
2017-01-05 14:24:38 +0800

    add all images

--- run.sh
+++ run.sh
@@ -1,14 +1,44 @@
 ##morph 2-4
-python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/60-4-inter/output%s.jpg' --output 'examples/morph/2-4/output-60-4.jpg' --style-blend-weights 60 4
+#python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/60-4-inter/output%s.jpg' --output 'examples/morph/2-4/output-60-4.jpg' --style-blend-weights 60 4
 
 ##morph 2-4
-python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/40-24-inter/output%s.jpg' --output 'examples/morph/2-4/output-40-24.jpg' --style-blend-weights 40 24
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/56-8-inter/output%s.jpg' --output 'examples/morph/2-4/output-56-8.jpg' --style-blend-weights 56 8
 
 ##morph 2-4
-python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/32-32-inter/output%s.jpg' --output 'examples/morph/2-4/output-32-32.jpg' --style-blend-weights 32 32
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/52-12-inter/output%s.jpg' --output 'examples/morph/2-4/output-52-12.jpg' --style-blend-weights 52 12
 
 ##morph 2-4
-python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/20-44-inter/output%s.jpg' --output 'examples/morph/2-4/output-20-44.jpg' --style-blend-weights 20 44
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/48-16-inter/output%s.jpg' --output 'examples/morph/2-4/output-48-16.jpg' --style-blend-weights 48 16
 
 ##morph 2-4
-python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/4-60-inter/output%s.jpg' --output 'examples/morph/2-4/output-4-60.jpg' --style-blend-weights 4 60
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/44-20-inter/output%s.jpg' --output 'examples/morph/2-4/output-44-20.jpg' --style-blend-weights 44 20
+
+##morph 2-4
+#python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/40-24-inter/output%s.jpg' --output 'examples/morph/2-4/output-40-24.jpg' --style-blend-weights 40 24
+
+##morph 2-4
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/36-28-inter/output%s.jpg' --output 'examples/morph/2-4/output-36-28.jpg' --style-blend-weights 3628
+
+##morph 2-4
+#python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/32-32-inter/output%s.jpg' --output 'examples/morph/2-4/output-32-32.jpg' --style-blend-weights 32 32
+
+##morph 2-4
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/28-36-inter/output%s.jpg' --output 'examples/morph/2-4/output-28-36.jpg' --style-blend-weights 2836
+
+##morph 2-4
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/24-40-inter/output%s.jpg' --output 'examples/morph/2-4/output-24-40.jpg' --style-blend-weights 24 40
+
+##morph 2-4
+#python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/20-44-inter/output%s.jpg' --output 'examples/morph/2-4/output-20-44.jpg' --style-blend-weights 20 44
+
+##morph 2-4
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/16-48-inter/output%s.jpg' --output 'examples/morph/2-4/output-16-48.jpg' --style-blend-weights 16 48
+
+##morph 2-4
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/12-52-inter/output%s.jpg' --output 'examples/morph/2-4/output-12-52.jpg' --style-blend-weights 12 52
+
+##morph 2-4
+python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/8-56-inter/output%s.jpg' --output 'examples/morph/2-4/output-8-56.jpg' --style-blend-weights 8 56
+
+##morph 2-4
+#python neural_style.py --content 'examples/morph/2-4/1-content.jpg' --styles 'examples/morph/2-4/2-style.jpg' 'examples/morph/2-4/5-style.jpg' --iterations 800 --checkpoint-iterations 100 --checkpoint-output 'examples/morph/2-4/4-60-inter/output%s.jpg' --output 'examples/morph/2-4/output-4-60.jpg' --style-blend-weights 4 60
